4df81c627b11200447219286d1acc47d87f8a792
lawrient <sandincl.d@gmail.com>
2025-03-05 05:14:39 +0800

    import echo

--- auth-server/main.go
+++ auth-server/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import "github.com/labstack/echo/v4"
 
 func main() {
-	fmt.Println("Hello World!")
+	ec := echo.New()
+	ec.GET("/", func(c echo.Context) error {
+		return c.String(200, "Hello WOlrd")
+	})
+
+	ec.Logger.Fatal(ec.Start(":3000"))
 }
